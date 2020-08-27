ce26a8998ed2b05a5a7ed18ac6a21b0e47eae77d
yassix-coder <danielfrsf@gmail.com>
2020-08-27 13:24:30 +0100

    yassix

--- src/App.js
+++ src/App.js
@@ -1,26 +1,75 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
+import Tmdb from './Tmdb';
 import './App.css';
+import MovieRow from './components/MovieRow';
+import FeaturedMovie from './components/FeaturedMovie';
+import Header from './components/Header.js';
 
-function App() {
+export default () => {
+
+  const [movieList, setMovieList] = useState([]);
+  const [featuredData, setFeaturedData] = useState(null);
+  const [blackHeader, setBlackHeader] = useState(false);
+
+  useEffect(() => {
+    const loadAll = async () => {
+      // Pegar a lista Total
+      let list = await Tmdb.getHomeList();
+      setMovieList(list);
+
+      // Pegar o Featured
+      let originals = list.filter(i=> i.slug === 'originals');
+      let randomChosen = Math.floor(Math.random() * (originals[0].items.results.length));
+      let chosen = originals[0].items.results[randomChosen];
+      let chosenInfo = await Tmdb.getMovieInfo(chosen.id, 'tv');
+      setFeaturedData(chosenInfo);
+    };
+
+    loadAll();
+  }, []);
+
+    useEffect(() =>{
+      const scrollListener = () => {
+        if (window.scrollY > 10) {
+          setBlackHeader(true);
+        } else {
+          setBlackHeader(false);
+        }
+      }
+
+      window.addEventListener('scroll', scrollListener);
+      return () => {
+        window.removeEventListener('scroll', scrollListener);
+      }
+    }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <div className="page">
 
-export default App;
+      <Header black={blackHeader}/>
+
+      {featuredData && 
+        <FeaturedMovie item={featuredData} />
+      }
+
+      <section className="lists">
+        {movieList.map((item, key)=> (
+          <MovieRow key={key} title={item.title} items={item.items}/>
+        ))}
+      </section>
+
+      <footer>
+        Feito com <span role="img" aria-label='coração'>❤️</span> Pelo Yassix<br/>
+        Direitos de imagem para Netflix<br/>
+        Dados pegos pelo site Themoviedb.org
+      </footer>
+
+      {movieList.length <= 0 &&
+      <div className="loading">
+        <img src="https://www.filmelier.com/pt/br/news/wp-content/uploads/2020/03/netflix-loading.gif" alt="Carregando"/>
+      </div>
+      }
+    </div>
+  )
+};
+
